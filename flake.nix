6880218d265f80cd622d9f408e52f001a1aee590
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2023-09-11 14:16:03 +0000

    flake

--- flake.nix
+++ flake.nix
@@ -19,8 +19,8 @@
     # Shameless plug: looking for a way to nixify your themes and make
     # everything match nicely? Try nix-colors!
     # nix-colors.url = "github:misterio77/nix-colors";
-    inputs.disko.url = "github:nix-community/disko";
 
+    disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
   };
 
